55215be9bc1f75dd3f44f52d35754d9ac74c59e9
nimna nilwala <nimna.nilwala555@gmail.com>
2023-11-23 00:23:49 +0530

    Add project files.

---
+++ cSharpSwapNumbers1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cSharpSwapNumbers1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.Write("Enter the number 1...: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the number 2...: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+
+            Console.WriteLine("number 1 is " + num1 + " and number is = " + num2);
+        }
+    }
+}
